f53054fc59e6cd15ad149237e0ce5f09c70b32d7
Matthias Mac <matthias-mac@refineddigital.co.nz>
2020-05-19 11:17:34 +0200

    updates

--- src/Mailchimp/Module/Classes/Process.php
+++ src/Mailchimp/Module/Classes/Process.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace RefinedDigital\ProductManager\Module\Classes;
+namespace RefinedDigital\FormBuilder\Mailchimp\Module\Classes;
 
 use RefinedDigital\FormBuilder\Module\Contracts\FormBuilderCallbackInterface;
 use RefinedDigital\FormBuilder\Module\Http\Repositories\FormBuilderRepository;
+use RefinedDigital\FormBuilder\Module\Http\Repositories\FormsRepository;
+use Newsletter;
 
 
 class Process implements FormBuilderCallbackInterface {
@@ -11,5 +13,33 @@ class Process implements FormBuilderCallbackInterface {
     public function run($request, $form) {
         $formBuilderRepository = new FormBuilderRepository();
         $formBuilderRepository->compileAndSend($request, $form);
+        $formRepo = new FormsRepository($formBuilderRepository);
+        $fields = $formRepo->formatFieldsByName($request, $form);
+
+        $firstName = '';
+        $lastName = '';
+
+        if ($fields->Name) {
+            $name = explode(' ', $fields->Name);
+            $firstName = $name[0];
+            if (isset($name[1])) {
+                $lastName = $name[1];
+            }
+        }
+
+        if (isset($fields->{'First Name'}) && $fields->{'First Name'}) {
+            $firstName = $fields->{'First Name'};
+        }
+
+        if (isset($fields->{'Last Name'}) && $fields->{'Last Name'}) {
+            $lastName = $fields->{'Last Name'};
+        }
+
+        $mergeTags = [
+            'FNAME' => $firstName,
+            'LNAME' => $lastName
+        ];
+
+        Newsletter::subscribeOrUpdate($fields->Email, $mergeTags);
     }
 }
---
+++ src/Mailchimp/Module/Providers/FormBuilderMailchimpServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RefinedDigital\PaymentGateways\Stripe\Module\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormBuilderMailchimpServiceProvider extends ServiceProvider
+{
+}
